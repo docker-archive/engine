ec56e75e132d683884c26300c4ee2761ed39215f
Sebastiaan van Stijn <github@gone.nl>
2018-10-23 13:32:28 +0200

    (backport) adjust API version to 1.38, because fix is backported
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- integration/container/exec_test.go
+++ integration/container/exec_test.go
@@ -18,7 +18,7 @@ import (
 
 // TestExecWithCloseStdin adds case for moby#37870 issue.
 func TestExecWithCloseStdin(t *testing.T) {
-	skip.If(t, versions.LessThan(testEnv.DaemonAPIVersion(), "1.39"), "broken in earlier versions")
+	skip.If(t, versions.LessThan(testEnv.DaemonAPIVersion(), "1.38"), "broken in earlier versions")
 	defer setupTest(t)()
 
 	ctx := context.Background()
